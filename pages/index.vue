bb985b4c74b96f11c881dbbed2e8296bae2be3f2
shoshi <shoshiroberts@gmail.com>
2018-11-13 16:08:50 -0500

    Loop video

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <video autoplay>
+    <video autoplay loop>
       <source src="~assets/videos/earth.mp4" type="video/mp4">
       Sorry, your browser does not support HTML5 video
     </video>
