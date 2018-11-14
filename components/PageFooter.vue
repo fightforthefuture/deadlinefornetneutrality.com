5f22e249167633a5ce25d26583e473d28b94489e
shoshi <shoshiroberts@gmail.com>
2018-11-14 16:28:09 -0500

    Smaller footer text

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -2,15 +2,15 @@
   <div class="page-footer text-center sml-push-y4 med-push-y8">
     <div class="sml-pad-y4">
       <div class="wrapper">
-        <h3>Built by:</h3>
+        <h6>Built by:</h6>
         <a href="https://www.fightforthefuture.org">
           <img src="~assets/images/fftf-logo.svg" class="grid-center"
                alt="Fight for the Future logo" />
         </a>
 
-        <h3 class="sml-push-y2 med-push-y3">
+        <h6 class="sml-push-y2 med-push-y3">
           For press inquiries, please contact us at:
-        </h3>
+        </h6>
         <p class="sml-push-y1 tuck-bottom">
           <small>
             <a href="tel://9788526457">978-852-6457</a> or
